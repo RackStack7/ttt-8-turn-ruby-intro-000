ecc78d2d1dd9744dc93383628ae6a653de39e06a
James Daaran <jamesdaaran@gmail.com>
2021-04-03 16:14:41 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,53 @@
+#display_board
+def display_board(board=[" "," "," "," "," "," "," "," "," "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#valid_move?
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken(board, index) == false or position_taken(board, index) == nil
+    true
+  elsif index.between?(0,8) && position_taken(board, index) == true
+    false
+  else index < 0 or index > 8
+    false
+  end
+end
+
+def position_taken(board,index)
+  if board[index] == "" or board[index] == " " or board[index] == nil
+    false
+  else board[index] == "X" or board[index] == "O"
+    true
+  end
+end
+
+#move
+def input_to_index(input)
+input_to_index = input.to_i
+index = input_to_index - 1
+end
+  def update_array_at_with(board, index, value)
+  board[index] = value
+  #end
+  update_array_at_with(board, index, "X")
+  display_board(board)
+end
+
+#turn
+def turn(board)
+  puts "Please end 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  move = valid_move?(board, index)
+  #if move == true
+    update_array_at_with(board, index, "X")
+  #else
+  #  puts "Invalid move, please try again:"
+  #  bin/turn
+  #end
+end
